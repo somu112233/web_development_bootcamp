362121455d6f525cdcde64afd1c4ab7ea803e8d3
Soma Sekhar <gudivadasomasekhar@gmail.com>
2026-07-10 02:52:39 +0530

    day 10 JavaScript loops practice  completed

---
+++ day 10/script.js
@@ -0,0 +1,77 @@
+{for (let i= 1; i<=5; i++) {
+    console.log(i);
+}
+
+let i = 1;
+while (i <=5){
+    console.log(i); i++;
+}
+}
+
+
+{
+
+let i = 1;
+
+do { 
+    console.log(i); i++;
+} while (i <=5);
+
+}
+
+{
+    for (let i = 1; i <= 10; i++) {
+    if (i === 6) {
+        break;
+    }
+    console.log(i);
+}
+}
+
+for (let i = 1; i <= 5; i++) {
+    if (i === 3) {
+        continue;
+    }
+    console.log(i);
+}
+
+
+
+for (let i =1; i<=20; i++) {
+    console.log(i);
+}
+
+for (let i = 20; i >= 1; i--){
+    console.log(i);
+
+}
+
+for (let i = 2; i <= 50;  i +=2){
+    console.log(i);
+}
+
+for (let i = 3; i <= 50;  i +=2){
+    console.log(i);
+}
+
+for (let i = 7*1; i <= 7*10;  i +=7){
+    console.log(i);
+}
+{
+let sum = 0;
+for (let i=1; i<= 100; i++){
+    sum += i;
+    console.log(sum);
+}
+}
+
+{
+let sum = 0;
+for (let i=1; i<= 30; i++){
+    sum += i;}
+    console.log(sum);
+}
+
+for (let i = 10; i >= 1; i--){
+    console.log(i);
+}
